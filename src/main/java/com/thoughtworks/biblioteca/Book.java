254e752e296c45df67c9dd55be20a305529478ac
brianpaff1992 <brianpaff1992@gmail.com>
2015-09-26 21:26:47 -0500

    Refactored obscure variable names
    Changed “pir” in Main to player input reader, player in .equals() in
    player to playerToCompareTo, p in playerTurn to playerNumber and s in
    playerTurn to  playerToBeSwitched

--- src/main/java/com/thoughtworks/biblioteca/Book.java
+++ src/main/java/com/thoughtworks/biblioteca/Book.java
@@ -10,16 +10,26 @@ public class Book {
     private String author;
     private String year;
     private PrintStream printStream;
+    private boolean isCheckedOut;
 
     public Book(String title, String author, String year, PrintStream printStream){
         this.title = title;
         this.author = author;
         this.year = year;
         this.printStream =printStream;
+        isCheckedOut = false;
     }
 
     public void printDetails() {
         String bookDetails = String.format("%-30s| %-30s| %s", title, author, year);
         printStream.println(bookDetails);
+    }
+
+    public void checkOut() {
+        isCheckedOut = true;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
     }
 }
